abed41d22b14e79882b98adab056196309dea9e8
tuvoksg1 <tuvoksg1@hotmail.com>
2016-08-14 09:57:17 +0100

    Defined custom login module (view not working)

---
+++ NancySelfHost/Modules/LoginModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace NancySelfHost.Modules
+{
+    public class LoginModule : NancyModule
+    {
+        public LoginModule()
+        {
+            Get["/core/custom/login", runAsync: true] = async (parameters, token) =>
+            {
+                Console.WriteLine("Doing work");
+                await Task.Delay(0);
+
+                return Task.FromResult(View["login"]);
+            };
+
+            Post["/core/custom/login", runAsync: true] = async (parameters, token) =>
+            {
+                Console.WriteLine("Doing work");
+                await Task.Delay(0);
+
+                return Task.FromResult(View["index"]);
+            };
+        }
+    }
+}
