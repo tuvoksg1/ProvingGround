5080fc26f0219dd6bf6fa2ff8b981a515e2f1669
tuvoksg1 <tuvoksg1@hotmail.com>
2016-08-14 06:21:21 +0100

    Updated Id User

---
+++ ElasticConsole/Models/IdentityUser.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElasticConsole.Models
+{
+    /// <summary>
+    /// Represents a user
+    /// </summary>
+    public class IdentityUser
+    {
+        /// <summary>
+        /// Default constructor 
+        /// </summary>
+        public IdentityUser()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        /// <summary>
+        /// Constructor that takes user name as argument
+        /// </summary>
+        /// <param name="userName"></param>
+        public IdentityUser(string userName)
+            : this()
+        {
+            UserName = userName;
+        }
+
+        /// <summary>
+        /// User ID
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tenant identifier.
+        /// </summary>
+        /// <value>
+        /// The tenant identifier.
+        /// </value>
+        public string TenantId { get; set; }
+
+        /// <summary>
+        /// User's name
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        ///     Email
+        /// </summary>
+        public virtual string Email { get; set; }
+
+        /// <summary>
+        ///     True if the email is confirmed, default is false
+        /// </summary>
+        public virtual bool EmailConfirmed { get; set; }
+
+        /// <summary>
+        ///     The salted/hashed form of the user password
+        /// </summary>
+        public virtual string PasswordHash { get; set; }
+
+        /// <summary>
+        ///     A random value that should change whenever a users credentials have changed (password changed, login removed)
+        /// </summary>
+        public virtual string SecurityStamp { get; set; }
+
+        /// <summary>
+        ///     PhoneNumber for the user
+        /// </summary>
+        public virtual string PhoneNumber { get; set; }
+
+        /// <summary>
+        ///     True if the phone number is confirmed, default is false
+        /// </summary>
+        public virtual bool PhoneNumberConfirmed { get; set; }
+
+        /// <summary>
+        ///     Is two factor enabled for the user
+        /// </summary>
+        public virtual bool TwoFactorEnabled { get; set; }
+
+        /// <summary>
+        ///     DateTime in UTC when lockout ends, any time in the past is considered not locked out.
+        /// </summary>
+        public virtual DateTime? LockoutEndDateUtc { get; set; }
+
+        /// <summary>
+        ///     Is lockout enabled for this user
+        /// </summary>
+        public virtual bool LockoutEnabled { get; set; }
+
+        /// <summary>
+        ///     Used to record failures for the purposes of lockout
+        /// </summary>
+        public virtual int AccessFailedCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the claims.
+        /// </summary>
+        /// <value>
+        /// The claims.
+        /// </value>
+        public List<ClaimModel> Claims { get; set; }
+    }
+}
