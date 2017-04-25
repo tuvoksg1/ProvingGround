810f798050ae0e33ab78d5a2f6ea26498d0b9ac5
tuvoksg1 <tuvoksg1@hotmail.com>
2017-04-25 13:12:40 +0100

    Added kafka spam

--- Messenger.Console/Program.cs
+++ Messenger.Console/Program.cs
@@ -1,4 +1,7 @@
-﻿using Messenger.Console.Gods;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Messenger.Console.Gods;
 
 namespace Messenger.Console
 {
@@ -9,8 +12,31 @@ namespace Messenger.Console
             //var god = new Ares();
             //god.AddMessages();
             //god.ListMessages();
-            JsonBuilder.Build();
+            //JsonBuilder.Build();
+            RunPost();
             System.Console.ReadLine();
+        }
+
+        private static void RunPost()
+        {
+            try
+            {
+                Postman.Spam();
+
+                //var taskList = new List<Task> { Task.Factory.StartNew(() => Postman.Spam()).Unwrap() };
+                //Task.WaitAll(taskList.ToArray());
+
+                //foreach (var runTask in taskList)
+                //    System.Console.WriteLine((runTask as Task<string>)?.Result);
+            }
+            catch (Exception error)
+            {
+                while (error != null)
+                {
+                    System.Console.WriteLine(error.Message);
+                    error = error.InnerException;
+                }
+            }
         }
     }
 }
